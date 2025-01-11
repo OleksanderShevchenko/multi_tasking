8706546559d6df2cb58475e6dd1086291ce27e85
Oleksander  Shevchenko <oleksandr.shevchenko@damen.com>
2025-01-11 21:32:07 +0200

    Simplify example - decrease number of iteration in cpu bound method, do not estimate time, as far as we are not waiting for all thread done yet.

--- src/multi_tasking/create_threads/create_simple_thread.py
+++ src/multi_tasking/create_threads/create_simple_thread.py
@@ -11,36 +11,24 @@ def io_bound_task(number: int) -> None:
 def cpu_bound_task(number: int) -> None:
     print(f"\nStart cpu_bound task #{number}")
     int_val = 1
-    for _ in range(1_000_000_000):
+    for _ in range(20_000_000):
         int_val += 1
     print(f"\nFinish cpu_bound task #{number}")
 
 
 if __name__ == "__main__":
     # do sequentially
-    start_time = time.perf_counter()
     for i in range(5):
         io_bound_task(i)
-    end_time = time.perf_counter()
-    print(f"Sequential io_bound done in {end_time - start_time} seconds.")
 
-    start_time = time.perf_counter()
     for i in range(5):
         cpu_bound_task(i)
-    end_time = time.perf_counter()
-    print(f"Sequential cpu_bound done in {end_time - start_time} seconds.")
-
-    # do threaded
-    start_time = time.perf_counter()
+    print("\n *** Run the same in threads ***")
+    # run in threads
     for i in range(5):
         t = threading.Thread(target=io_bound_task, args=(i,))
         t.start()
-    end_time = time.perf_counter()
-    print(f"Threaded io_bound done in {end_time - start_time} seconds.")
 
-    start_time = time.perf_counter()
     for i in range(5):
         t = threading.Thread(target=cpu_bound_task, args=(i,))
         t.start()
-    end_time = time.perf_counter()
-    print(f"Threaded cpu_bound done in {end_time - start_time} seconds.")
