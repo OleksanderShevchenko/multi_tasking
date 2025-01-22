8132e76227350e55db272b0aae67697bb2c8002a
Олександр Шевченко <oleksander.shevchenko777@gmail.com>
2025-01-22 23:23:25 +0200

    Decrease num of iterations and add delay to get race condition.

--- src/multi_tasking/threads_synchronization_examples/donator_spender_no_synch.py
+++ src/multi_tasking/threads_synchronization_examples/donator_spender_no_synch.py
@@ -3,16 +3,18 @@ from threading import Thread
 
 
 class DonatorSpender():
-
-    money = 100
+    def __init__(self):
+        self.money = 100
 
     def spend(self) -> None:
-        for i in range(1_000_000_000):  # lot of loops to gt into raise condition
+        for i in range(100_000):  # lot of loops to gt into raise condition
+            time.sleep(0.00001)
             self.money -= 10
         print('Spend done!')
 
     def donate(self) -> None:
-        for i in range(1_000_000_000):
+        for i in range(100_000):
+            time.sleep(0.00001)
             self.money += 10
         print('Donate done!')
 
@@ -21,9 +23,10 @@ if __name__ == "__main__":
     ds = DonatorSpender()
     t1 = Thread(target=ds.donate, args=())
     t2 = Thread(target=ds.spend, args=())
+    start = time.perf_counter()
     t1.start()
     t2.start()
     while t1.is_alive() and t2.is_alive():
         time.sleep(1)
-
-    print(f"Finally we've got {ds.money}")
+    end = time.perf_counter()
+    print(f"Finally we've got {ds.money}! It takes {end-start} sec.")
