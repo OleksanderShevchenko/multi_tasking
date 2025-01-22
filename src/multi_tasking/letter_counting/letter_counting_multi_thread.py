4b2b016c6667c89535ded63c2d1f105a39a05794
Олександр Шевченко <oleksander.shevchenko777@gmail.com>
2025-01-22 20:49:21 +0200

    Add example for single and multi-thread functions for counting letters.

---
+++ src/multi_tasking/letter_counting/letter_counting_multi_thread.py
@@ -0,0 +1,42 @@
+import json
+from threading import Thread
+from urllib import request, error
+import time
+
+_counter = 0
+
+
+def count_letters(url: str, frequency: dict) -> None:
+    try:
+        response = request.urlopen(url)
+    except error.HTTPError as err:
+        print(f"\nurl={url} error: {err}")
+    else:
+        txt = str(response.read())
+        for l in txt:
+            letter = l.lower()
+            if letter in frequency.keys():
+                frequency[letter] += 1
+    finally:
+        global _counter
+        _counter += 1
+
+
+def main() -> None:
+    frequency = {}
+    for c in "abcdefghijklmnopqrstuvwxyz":
+        frequency[c] = 0
+    start = time.perf_counter()
+    for i in range(1000, 1061):
+        url = f"https://www.rfc-editor.org/rfc/rfc{i}.txt"
+        Thread(target=count_letters, args=(url, frequency)).start()
+    global _counter
+    while _counter < 61:
+        time.sleep(0.1)
+    end = time.perf_counter()
+    print(json.dumps(frequency, indent=4))
+    print(f"Done in {end - start} seconds")
+
+
+if __name__ == "__main__":
+    main()
---
+++ src/multi_tasking/letter_counting/letter_counting_single_thread.py
@@ -0,0 +1,33 @@
+import json
+from urllib import request, error
+import time
+
+
+def count_letters(url: str, frequency: dict) -> None:
+    try:
+        response = request.urlopen(url)
+    except error.HTTPError as err:
+        print(f"url={url} error: {err}")
+    else:
+        txt = str(response.read())
+        for l in txt:
+            letter = l.lower()
+            if letter in frequency.keys():
+                frequency[letter] += 1
+
+
+def main() -> None:
+    frequency = {}
+    for c in "abcdefghijklmnopqrstuvwxyz":
+        frequency[c] = 0
+    start = time.perf_counter()
+    for i in range(1000, 1061):
+        url = f"https://www.rfc-editor.org/rfc/rfc{i}.txt"
+        count_letters(url, frequency)
+    end = time.perf_counter()
+    print(json.dumps(frequency, indent=4))
+    print(f"Done in {end - start} seconds")
+
+
+if __name__ == "__main__":
+    main()
