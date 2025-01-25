0b8e84a291b6dae91125d7882e2fb1d42fd64627
Олександр Шевченко <oleksander.shevchenko777@gmail.com>
2025-01-25 14:32:13 +0200

    Make Lock instance to be local variable of main function. Make low letters in a whole downloaded text. Make acquire of the lock for whole iteration - for better performance. Protect global variable incrementation by lock too. Change while loop to protect global counter by lock and break on condition - releasing lock after break does not required as far as we've done there (but in other cases it could be important to release lock before break)

--- src/multi_tasking/letter_counting/letter_counting_multi_thread_mutex.py
+++ src/multi_tasking/letter_counting/letter_counting_multi_thread_mutex.py
@@ -3,39 +3,43 @@ from threading import Thread, Lock
 from urllib import request, error
 import time
 
-_counter = 0
-_mutex = Lock()
+_counter = 0  # variable that represent global shared memory
 
 
-def count_letters(url: str, frequency: dict) -> None:
+def count_letters(url: str, frequency: dict, _mutex: Lock) -> None:
     try:
         response = request.urlopen(url)
+        txt = str(response.read()).lower()
     except error.HTTPError as err:
         print(f"\nurl={url} error: {err}")
     else:
-        txt = str(response.read())
-        for l in txt:
-            letter = l.lower()
+        _mutex.acquire()
+        for letter in txt:
             if letter in frequency.keys():
-                global _mutex
-                _mutex.acquire()
                 frequency[letter] += 1
-                _mutex.release()
+        _mutex.release()
     finally:
+        _mutex.acquire()
         global _counter
         _counter += 1
+        _mutex.release()
 
 
 def main() -> None:
-    frequency = {}
+    frequency = {}  # variable that represent local shared memory
+    _mutex = Lock()
     for c in "abcdefghijklmnopqrstuvwxyz":
         frequency[c] = 0
     start = time.perf_counter()
     for i in range(1000, 1182):
         url = f"https://www.rfc-editor.org/rfc/rfc{i}.txt"
-        Thread(target=count_letters, args=(url, frequency)).start()
-    global _counter
-    while _counter < 182:
+        Thread(target=count_letters, args=(url, frequency, _mutex)).start()
+    while True:
+        _mutex.acquire()
+        global _counter
+        if _counter == 182:
+            break
+        _mutex.release()
         time.sleep(0.5)
     end = time.perf_counter()
     print(json.dumps(frequency, indent=4))
